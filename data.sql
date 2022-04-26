3529477702ece0525af941ea6c52e9d651bbb8cc
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-27 00:50:35 +0430

    Add a few more records to the animals table.

--- data.sql
+++ data.sql
@@ -58,3 +58,107 @@ VALUES      (4,
              5,
              '1',
              11); 
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (5,
+             'Charmander',
+             '2020-02-08',
+             0,
+             '0',
+             -11); 
+
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (6,
+             'Plantmon',
+             '2021-11-15',
+             2,
+             '1',
+             -5.7); 
+
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (7,
+             'Squirtle',
+             '1993-04-02',
+             3,
+             '0',
+             -12.13); 
+
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (8,
+             'Angemon',
+             '2005-06-12',
+             1,
+             '1',
+             -45); 
+
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (9,
+             'Boarmon',
+             '2005-06-07',
+             7,
+             '1',
+             20.4); 
+
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (10,
+             'Blossom',
+             '1998-10-13',
+             3,
+             '1',
+             17); 
+			 
+
+INSERT INTO animals
+            (id,
+             NAME,
+             date_of_birth,
+             escape_attempts,
+             neutered,
+             weight_kg)
+VALUES      (11,
+             'Ditto',
+             '2022-05-14',
+             4,
+             '1',
+             22); 
