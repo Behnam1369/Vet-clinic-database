9d2881f67fc0daff72a382b4f8716dd2a79220ef
behnam1369 <behnam.aghaali@yahoo.com>
2022-04-27 21:44:20 +0430

    Add owners and species tables and relations

--- schema.sql
+++ schema.sql
@@ -19,3 +19,34 @@ CREATE TABLE animals (
 /*Add a column species of type string to your animals table. Modify your schema.sql file.*/
 
 ALTER TABLE animals ADD species varchar(100);
+
+
+/*Create owners table*/
+
+CREATE TABLE owners (
+	id int GENERATED ALWAYS AS IDENTITY,
+	full_name varchar(250),
+	age int,
+	PRIMARY KEY (id) 
+);
+
+
+/*Create species table*/
+
+CREATE TABLE species (
+	id int GENERATED ALWAYS AS IDENTITY,
+	name varchar(100),
+	PRIMARY KEY (id)
+);
+
+
+/*Modify animals table*/
+
+ALTER TABLE animals ADD PRIMARY KEY (id); 
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id int REFERENCES species (id);
+
+ALTER TABLE animals ADD owner_id  int REFERENCES owners (id);
+
