eae558da0edd51e633db91acd1a6467b849e3224
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-03 21:24:37 +0430

    Add more records to assess peerformance and add more descriptive comments

--- schema.sql
+++ schema.sql
@@ -79,7 +79,19 @@ CREATE TABLE visits (
 );
 
 
--- 1
+/*Add email column to the owners table*/
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+
+-- Query 1: SELECT COUNT(*) FROM visits where animal_id = 4;
 CREATE INDEX IF NOT EXISTS animal_id
     ON public.visits USING btree
     (animal_id ASC NULLS LAST)
@@ -88,7 +100,7 @@ CREATE INDEX IF NOT EXISTS animal_id
 ALTER TABLE IF EXISTS public.visits
     CLUSTER ON animal_id;
 
--- 2
+-- Query 2: SELECT * FROM visits where vet_id = 2;
 CREATE INDEX IF NOT EXISTS vet_id_index
     ON public.visits USING btree
     (vet_id ASC NULLS LAST)
@@ -99,7 +111,7 @@ ALTER TABLE IF EXISTS public.visits
     CLUSTER ON vet_id_index;
 
 
--- 3
+-- Query 3: SELECT * FROM owners where email = 'owner_18327@mail.com';
 CREATE INDEX IF NOT EXISTS owner_index
     ON public.owners USING btree
     (email COLLATE pg_catalog."default" ASC NULLS LAST)
